03a8dd307c628d0edfa37ffb02278cef9b9db9b3
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-05-12 21:15:10 +0600

    Coffee Card Add Update Delete Done using Server

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/updateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffee')
+  },
+  {
+    path: "addcoffee",
+    element: <AddCoffee></AddCoffee>
+  },
+  {
+    path: "updatecoffee/:id",
+    element: <UpdateCoffee></UpdateCoffee>,
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
